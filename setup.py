c49a5ced5670c8cb1a8ce0a160fb54312f4189c5
Alfredo <alfredo.delafuente.b@gmail.com>
2016-09-17 18:11:32 -0500

    Upload setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Synthetic-Text-Generator',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='MIT License',
+    author='Alfredo De la Fuente',
+    author_email='alfredo.delafuente.b@gmail.com',
+    description=''
+)
